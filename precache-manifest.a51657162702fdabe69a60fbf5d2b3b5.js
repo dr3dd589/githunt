2faf2fe54a30bba31368da7ce25c415f553227bd
dr3dd589 <skumar1@mt.iitr.ac.in>
2023-11-28 21:12:51 -0800

    Updates

---
+++ precache-manifest.a51657162702fdabe69a60fbf5d2b3b5.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12de239b43bd421bbd76500295f058c2",
+    "url": "/githunt/index.html"
+  },
+  {
+    "revision": "a6b61358bd493149dbae",
+    "url": "/githunt/static/css/2.892b497a.chunk.css"
+  },
+  {
+    "revision": "2932fbf647a901b00ba0",
+    "url": "/githunt/static/css/main.90b2da25.chunk.css"
+  },
+  {
+    "revision": "a6b61358bd493149dbae",
+    "url": "/githunt/static/js/2.b5f9f1d1.chunk.js"
+  },
+  {
+    "revision": "4150470712be826fb0330d583499dea9",
+    "url": "/githunt/static/js/2.b5f9f1d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2932fbf647a901b00ba0",
+    "url": "/githunt/static/js/main.2592a560.chunk.js"
+  },
+  {
+    "revision": "1d00c21c22f09586a585",
+    "url": "/githunt/static/js/runtime-main.b0ec4c0c.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/githunt/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/githunt/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/githunt/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/githunt/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/githunt/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
